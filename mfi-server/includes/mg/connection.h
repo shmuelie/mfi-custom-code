9310f2c0213b271794a3d242788deb899abd2358
Shmueli Englard <shmueli.yosef@englard.net>
2024-01-15 21:31:44 -0800

    Created string join method and added onnection::http_reply

--- mfi-server/includes/mg/connection.h
+++ mfi-server/includes/mg/connection.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <string>
+#include <map>
 #include "mongoose.h"
 
 namespace mg {
 	class connection {
 	public:
 		connection(mg_connection* c) noexcept;
+
+		void http_reply(int status_code, const std::map<std::string, std::string>& headers, const std::string& body) const noexcept;
 	private:
 		mg_connection* _connection;
 	};
--- mfi-server/src/mg/connection.cpp
+++ mfi-server/src/mg/connection.cpp
@@ -1,6 +1,17 @@
 #include "mg/connection.h"
+#include <vector>
+#include "join.h"
 
+using namespace std;
 using namespace mg;
 
 connection::connection(mg_connection* c) noexcept : _connection(c) {
+}
+
+void connection::http_reply(int status_code, const map<string, string>& headers, const string& body) const noexcept {
+	vector<string> headersVector;
+	for (const map<string, string>::const_iterator::reference pair : headers) {
+		headersVector.push_back(pair.first + ": " + pair.second);
+	}
+	mg_http_reply(_connection, status_code, join(headersVector.cbegin(), headersVector.cend(), "\r\n", "\r\n").c_str(), body.c_str());
 }
