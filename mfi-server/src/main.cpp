644d73f85ad106ef68fcb6cbc1f721ac415f137d
Shmueli Englard <shmueli.yosef@englard.net>
2024-01-15 20:06:10 -0800

    Basic calling to mfi_server

--- mfi-server/src/main.cpp
+++ mfi-server/src/main.cpp
@@ -1,3 +1,11 @@
+#include "mfi_server.h"
+
+using namespace std::chrono_literals;
+
 int main(int argc, char* argv[]) {
-	return 0;
+	mfi_server server{};
+	server.listen("http://0.0.0.0:8000");
+	for (;;) {
+		server.poll(1000s);
+	}
 }
