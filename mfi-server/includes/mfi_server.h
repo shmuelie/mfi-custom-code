d64726ba4457ccef44acd5b0e0e35c5786f2710a
Shmueli Englard <shmueli.yosef@englard.net>
2024-01-17 22:19:51 -0800

    Refactor mfi_server to have deadicated method for each endpoint

--- mfi-server/includes/mfi_server.h
+++ mfi-server/includes/mfi_server.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 #include "mg/http_server.h"
 #include "mfi.h"
 
@@ -12,5 +13,8 @@ protected:
 	virtual mg::http_response http_handler(const mg::http_message& message) noexcept override;
 
 private:
+	mg::http_response status_handler() noexcept;
+	mg::http_response sensor_handler(const std::string& method, const std::vector<std::string>& captures, const std::string& body) noexcept;
+
 	mfi::board _board{};
 };
--- mfi-server/src/mfi_server.cpp
+++ mfi-server/src/mfi_server.cpp
@@ -1,5 +1,4 @@
 #include "mfi_server.h"
-#include <vector>
 #include "join.h"
 
 using namespace std;
@@ -13,53 +12,65 @@ static const string to_json(sensor sensor) {
 	return "{\"power\":" + to_string(sensor.power()) + ",\"current\":" + to_string(sensor.current()) + ",\"voltage\":" + to_string(sensor.voltage()) + ",\"relay\":" + (sensor.relay() ? "true" : "false") + "}";
 }
 
+http_response mfi_server::status_handler() noexcept {
+	const vector<sensor>& sensors = _board.sensors();
+	vector<string> sensorsJson{};
+	for (sensor sensor : _board.sensors()) {
+		sensorsJson.push_back(to_json(sensor));
+	}
+	return { 200, map<string, string>{
+		{"Content-Type", "application/json"}
+	}, ("[" + join(sensorsJson.cbegin(), sensorsJson.cend(), ",") + "]") };
+}
+
+http_response mfi_server::sensor_handler(const string& method, const vector<string>& captures, const string& body) noexcept {
+	if (captures.size() != 1) {
+		return { 400, "Invalid path" };
+	}
+
+	uint8_t sensorId;
+	try {
+		sensorId = static_cast<uint8_t>(stoul(captures.at(0), nullptr, 10));
+	}
+	catch (invalid_argument& ex) {
+		return { 400, ex.what() };
+	}
+
+	if (sensorId >= _board.sensors().size()) {
+		return { 400, "Invalid Sensor ID" };
+	}
+
+	sensor sensor = _board.sensors().at(sensorId);
+	if (method == "GET") {
+		return { 200, map<string, string>{
+			{"Content-Type", "application/json"}
+		}, to_json(sensor) };
+	}
+	else if (method == "POST") {
+		if (body == "on") {
+			sensor.relay(true);
+			return 200;
+		}
+		else if (body == "off") {
+			sensor.relay(false);
+			return 200;
+		}
+		else {
+			return { 400, "Unexpected body " + body };
+		}
+	}
+	else {
+		return { 405, "Unexpected method " + method };
+	}
+}
+
 http_response mfi_server::http_handler(const http_message& message) noexcept {
 	vector<string> captures{};
 	if (message.match_uri("/api/v2/status")) {
-		const vector<sensor>& sensors = _board.sensors();
-		vector<string> sensorsJson{};
-		for (sensor sensor : _board.sensors()) {
-			sensorsJson.push_back(to_json(sensor));
-		}
-		return { 200, map<string, string>{
-			{"Content-Type", "application/json"}
-		}, ("[" + join(sensorsJson.cbegin(), sensorsJson.cend(), ",") + "]") };
+		return status_handler();
 	}
 	else if (message.match_uri<1>("/api/v2/sensor/*", captures)) {
-		try {
-			uint8_t sensorId = static_cast<uint8_t>(stoul(captures.at(0), nullptr, 10));
-			if (sensorId < _board.sensors().size()) {
-				sensor sensor = _board.sensors().at(sensorId);
-				if (message.method() == "GET") {
-					return { 200, map<string, string>{
-						{"Content-Type", "application/json"}
-					}, to_json(sensor) };
-				}
-				else if (message.method() == "POST") {
-					const string body = message.body();
-					if (body == "on") {
-						sensor.relay(true);
-						return 200;
-					}
-					else if (body == "off") {
-						sensor.relay(false);
-						return 200;
-					}
-					else {
-						return { 400, "Unexpected body " + body };
-					}
-				}
-				else {
-					return { 405, "Unexpected method " + message.method() };
-				}
-			}
-			else {
-				return { 400, "Invalid Sensor ID" };
-			}
-		}
-		catch (invalid_argument& ex) {
-			return { 400, ex.what() };
-		}
+		return sensor_handler(message.method(), captures, message.body());
 	}
 	else {
 		return 404;
