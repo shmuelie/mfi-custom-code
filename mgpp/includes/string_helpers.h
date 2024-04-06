787c6fe9f9a999ec9896da828330bd70c81ab260
Shmueli Englard <shmueli.yosef@englard.net>
2024-04-06 13:51:01 -0700

    Extract mongoose C++ wrapper into its own project

---
+++ mgpp/includes/string_helpers.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <string>
+#include <optional>
+#include <stdexcept>
+#include <sstream>
+#include <iostream>
+#include <vector>
+
+namespace string_helpers {
+	template<typename InputIt>
+	static std::string join(InputIt first,
+		InputIt last,
+		const std::string& separator = ", ",
+		const std::string& concluder = "")
+	{
+		if (first == last)
+		{
+			return concluder;
+		}
+
+		std::stringstream ss{};
+		ss << *first;
+		++first;
+
+		while (first != last)
+		{
+			ss << separator;
+			ss << *first;
+			++first;
+		}
+
+		ss << concluder;
+
+		return ss.str();
+	}
+
+	template<typename NumberType>
+	static std::optional<NumberType> try_stoul(const std::string& __str, size_t* __idx = nullptr, int __base = 10) {
+		try {
+			return static_cast<NumberType>(std::stoul(__str, __idx, __base));
+		}
+		catch (std::invalid_argument) {
+			return std::nullopt;
+		}
+		catch (std::out_of_range) {
+			return std::nullopt;
+		}
+	}
+
+	static bool is_number(const std::string& str)
+	{
+		// `std::find_first_not_of` searches the string for the first character
+		// that does not match any of the characters specified in its arguments
+		return !str.empty() && (str.find_first_not_of("[0123456789]") == std::string::npos);
+	}
+
+	static std::vector<std::string> split(const std::string& str, char delim)
+	{
+		auto i = 0;
+		std::vector<std::string> list;
+
+		auto pos = str.find(delim);
+
+		while (pos != std::string::npos)
+		{
+			list.push_back(str.substr(i, pos - i));
+			i = ++pos;
+			pos = str.find(delim, pos);
+		}
+
+		list.push_back(str.substr(i, str.length()));
+
+		return list;
+	}
+}
