5beee015557fd34011dbb996f27564270424aab4
Shmueli Englard <shmueli.yosef@englard.net>
2024-04-07 14:32:36 -0700

    Built more mqtt stuff

--- mgpp/includes/mg/mqtt_client.h
+++ mgpp/includes/mg/mqtt_client.h
@@ -5,6 +5,7 @@
 #include "mg/eventing_handler.h"
 #include "mg/mqtt_options.h"
 #include "mg/mqtt_message.h"
+#include "mg/timer.h"
 
 namespace mg {
 	class mqtt_client : public eventing_handler {
@@ -14,16 +15,16 @@ namespace mg {
 		explicit mqtt_client(const std::string& url, const mqtt_options& options) noexcept;
 		explicit mqtt_client(const std::shared_ptr<mg::manager>& manager, const std::string& url, const mqtt_options& options) noexcept;
 
-		void start() noexcept;
 		bool is_connected() const noexcept;
 	protected:
 		virtual void event_handler(const connection& connection, event event, void* event_data) noexcept override;
 		virtual void message_handler(const mqtt_message& message) noexcept = 0;
 		virtual void open_handler() noexcept = 0;
 	private:
-		static void _timer_handler(void* arg) noexcept;
+		void timer_handler() noexcept;
 		std::optional<connection> _connection;
 		std::string _url;
 		mqtt_options _starting_options;
+		std::shared_ptr<timer> _timer;
 	};
 }
--- mgpp/src/mg/mqtt_client.cpp
+++ mgpp/src/mg/mqtt_client.cpp
@@ -2,28 +2,36 @@
 #include "mg/manager.h"
 
 using namespace std;
+using namespace std::chrono_literals;
 using namespace mg;
 
 mqtt_client::mqtt_client(const string& url, const mqtt_options& options) noexcept : eventing_handler(), _url(url), _starting_options(options), _connection() {
+	_timer = make_shared<timer>(manager(), 3s, true, true, &mqtt_client::timer_handler);
 }
 
 mqtt_client::mqtt_client(const shared_ptr<mg::manager>& manager, const string& url, const mqtt_options& options) noexcept : eventing_handler(manager), _url(url), _starting_options(options), _connection() {
-}
-
-void mqtt_client::start() noexcept {
+	_timer = make_shared<timer>(this->manager(), 3s, true, true, &mqtt_client::timer_handler);
 }
 
 bool mqtt_client::is_connected() const noexcept {
 	return _connection.has_value();
 }
 
-void mqtt_client::_timer_handler(void* arg) noexcept {
-	mqtt_client* self = static_cast<mqtt_client*>(arg);
-
-	if (!self->is_connected()) {
-		optional<connection> c = self->manager()->connect(self, self->_url, self->_starting_options);
+void mqtt_client::timer_handler() noexcept {
+	if (!is_connected()) {
+		optional<connection> c = manager()->connect(this, _url, _starting_options);
 	}
 }
 
 void mqtt_client::event_handler(const connection& connection, event event, void* event_data) noexcept {
+	switch (event)
+	{
+	case mg::event::mqtt_open:
+		open_handler();
+		break;
+	case mg::event::mqtt_message:
+		mqtt_message message = static_cast<mg_mqtt_message*>(event_data);
+		message_handler(message);
+		break;
+	}
 }
