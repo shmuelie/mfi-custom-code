3ba58aec0859282807f3b875e9b3a5f0e457cd21
Shmueli Englard <shmueli.yosef@englard.net>
2024-04-07 17:54:01 -0700

    Fixes to mqtt client

--- mgpp/src/mg/mqtt_client.cpp
+++ mgpp/src/mg/mqtt_client.cpp
@@ -6,11 +6,16 @@ using namespace std::chrono_literals;
 using namespace mg;
 
 mqtt_client::mqtt_client(const string& url, const mqtt_options& options) noexcept : eventing_handler(), _url(url), _starting_options(options), _connection() {
-	_timer = make_shared<timer>(manager(), 3s, true, true, &mqtt_client::timer_handler);
+	const shared_ptr<mg::manager> manager = this->manager();
+	_timer = make_shared<timer>(manager, 3s, true, true, [this]() {
+		this->timer_handler();
+	});
 }
 
 mqtt_client::mqtt_client(const shared_ptr<mg::manager>& manager, const string& url, const mqtt_options& options) noexcept : eventing_handler(manager), _url(url), _starting_options(options), _connection() {
-	_timer = make_shared<timer>(this->manager(), 3s, true, true, &mqtt_client::timer_handler);
+	_timer = make_shared<timer>(manager, 3s, true, true, [this]() {
+		this->timer_handler();
+	});
 }
 
 bool mqtt_client::is_connected() const noexcept {
@@ -27,14 +32,20 @@ void mqtt_client::event_handler(const connection& connection, event event, void*
 	switch (event)
 	{
 	case mg::event::mqtt_open:
+	{
 		open_handler();
 		break;
+	}
 	case mg::event::mqtt_message:
+	{
 		mqtt_message message = static_cast<mg_mqtt_message*>(event_data);
 		message_handler(message);
 		break;
+	}
 	case mg::event::close:
+	{
 		_connection = nullopt;
 		break;
+	}
 	}
 }
