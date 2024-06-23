f7b7da5e806b461abb1dbe893fe2cf8caaf82478
Shmueli Englard <shmueli.yosef@englard.net>
2024-06-23 16:27:10 -0700

    Added LED to MQTT client

--- mfi-mqtt-client/includes/mfi_mqtt_client/mfi_device.h
+++ mfi-mqtt-client/includes/mfi_mqtt_client/mfi_device.h
@@ -2,9 +2,10 @@
 
 #include <vector>
 #include <memory>
-#include "mfi/board.h"
+#include "mfi.h"
 #include "mfi_mqtt_client/mfi_sensor.h"
 #include "hass_mqtt_device/core/mqtt_connector.h"
+#include "hass_mqtt_device/devices/on_off_light.h"
 
 namespace mfi_mqtt_client {
 	class mfi_device : public MQTTConnector {
@@ -12,8 +13,11 @@ namespace mfi_mqtt_client {
 		explicit mfi_device(const mfi::board& board, const std::string& server, int port, const std::string& username, const std::string& password) noexcept;
 
 		void update() noexcept;
+		mfi::led led() const noexcept;
 	private:
 		mfi::board _board;
+		mfi::led _led;
 		std::vector<std::shared_ptr<mfi_sensor>> _sensors;
+		std::shared_ptr<OnOffLightDevice> _light;
 	};
 }
--- mfi-mqtt-client/src/mfi_mqtt_client/mfi_device.cpp
+++ mfi-mqtt-client/src/mfi_mqtt_client/mfi_device.cpp
@@ -12,16 +12,24 @@ mfi_device::mfi_device(
 	const string& password) noexcept :
 	MQTTConnector(server, port, username, password, board.hostname()),
 	_board(board),
-	_sensors() {
+	_sensors(),
+	_led(),
+	_light() {
 	for (auto& sensor : board.sensors()) {
 		auto mfiSensor = make_shared<mfi_sensor>(sensor);
 		_sensors.push_back(mfiSensor);
 		registerDevice(mfiSensor);
 	}
+	_light = make_shared<OnOffLightDevice>("LED", [this](auto v) { this->led().color(v ? led_color::blue : led_color::off); }, "LED");
 }
 
 void mfi_device::update() noexcept {
 	for (auto& sensor : _sensors) {
 		sensor->update();
 	}
+	_light->update(_led.color() != led_color::off);
+}
+
+led mfi_device::led() const noexcept {
+	return _led;
 }
