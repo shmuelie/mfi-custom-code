c029d163ddff9134510be04e2fe3aaa4b89b1d6f
Shmueli Englard <shmueli.yosef@englard.net>
2024-06-24 21:52:45 -0700

    Fix parsing of port

--- mfi-mqtt-client/src/mfi_mqtt_client/options.cpp
+++ mfi-mqtt-client/src/mfi_mqtt_client/options.cpp
@@ -1,14 +1,24 @@
 #include "mfi_mqtt_client/options.h"
+#include "shmuelie/string_helpers.hpp"
 
 using namespace std;
 using namespace mfi_mqtt_client;
+using namespace shmuelie;
 
 options::options(const string& doc, const vector<string>& argv) noexcept : docopt_options(doc, argv) {
 	try {
 		auto& args = values();
 
 		_server = args.at("server").asString();
-		_port = static_cast<int>(args.at("port").asLong());
+
+		auto potentialPort = try_stoul<uint16_t>(args.at("--port").asString());
+		if (!potentialPort) {
+			add_error("Invalid port");
+		}
+		else {
+			_port = potentialPort.value();
+		}
+
 		_username = args.at("username").asString();
 		_password = args.at("password").asString();
 	}
