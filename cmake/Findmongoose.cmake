7d9d21a7813822aeabf56c09fef4ad8135ef2261
Shmueli Englard <shmueli.yosef@englard.net>
2024-07-07 16:09:03 -0700

    Changed to not directly include mongoose source in mgpp

---
+++ cmake/Findmongoose.cmake
@@ -0,0 +1,11 @@
+include(FetchContent)
+FetchContent_Declare(
+	mongoose
+	GIT_REPOSITORY https://github.com/cesanta/mongoose.git
+	GIT_TAG        52997c6c5170050148d23436d2ed398811f48c30 # 7.13
+)
+set(MONGOOSE_DIR ${FETCHCONTENT_BASE_DIR}/mongoose-src/)
+if(EXISTS ${MONGOOSE_DIR})
+	add_library(mongoose STATIC ${MONGOOSE_DIR}/mongoose.h ${MONGOOSE_DIR}/mongoose.c)
+	target_include_directories(mongoose PUBLIC ${MONGOOSE_DIR})
+endif()
