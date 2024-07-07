807ce00de1e16fb8282ed2cf4ab2c571820e50c4
Shmueli Englard <shmueli.yosef@englard.net>
2024-07-07 14:30:42 -0700

    Add some comments of sorts to editor script

--- edit-buildroot-config.sh
+++ edit-buildroot-config.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+NO_FORMAT="\033[0m"
+C_GREEN="\033[38;5;2m"
+
+echo -e "${C_GREEN}Load buildroot configuration${NO_FORMAT}"
 make -C ./buildroot/ BR2_EXTERNAL=../br2 O=../build/br mfi_defconfig
+echo -e "${C_GREEN}Start buildroot configuration editor${NO_FORMAT}"
 make -C ./buildroot/ BR2_EXTERNAL=../br2 O=../build/br nconfig
+echo -e "${C_GREEN}Save buildroot configuration${NO_FORMAT}"
 make -C ./buildroot/ BR2_EXTERNAL=../br2 O=../build/br savedefconfig
