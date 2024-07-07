819ec4648a715e5fcbc6607152574bedb9c38f76
Shmueli Englard <shmueli.yosef@englard.net>
2024-07-07 12:37:00 -0700

    Improve editor script

--- edit-buildroot-config.sh
+++ edit-buildroot-config.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-make -C buildroot BR2_EXTERNAL=../br2 O=../build/br nconfig
+make -C ./buildroot/ BR2_EXTERNAL=../br2 O=../build/br mfi_defconfig
+make -C ./buildroot/ BR2_EXTERNAL=../br2 O=../build/br nconfig
+make -C ./buildroot/ BR2_EXTERNAL=../br2 O=../build/br savedefconfig
