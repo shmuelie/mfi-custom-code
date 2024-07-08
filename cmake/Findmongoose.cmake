fd8fd4c894ae89b34a8254a1782a8ca5df8c0120
Shmueli Englard <shmueli.yosef@englard.net>
2024-07-07 21:22:14 -0700

    Fix find logic

--- cmake/Findmongoose.cmake
+++ cmake/Findmongoose.cmake
@@ -5,7 +5,9 @@ FetchContent_Declare(
 	GIT_TAG        52997c6c5170050148d23436d2ed398811f48c30 # 7.13
 )
 set(MONGOOSE_DIR ${FETCHCONTENT_BASE_DIR}/mongoose-src/)
-if(EXISTS ${MONGOOSE_DIR})
+if(EXISTS ${MONGOOSE_DIR}/mongoose.h)
 	add_library(mongoose STATIC ${MONGOOSE_DIR}/mongoose.h ${MONGOOSE_DIR}/mongoose.c)
 	target_include_directories(mongoose PUBLIC ${MONGOOSE_DIR})
+else()
+	FetchContent_MakeAvailable(mongoose)
 endif()
