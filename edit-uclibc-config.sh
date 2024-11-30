8c8589fbd52525d1040e5ff3207ac5854125a1c5
Shmueli Englard <shmueli.yosef@englard.net>
2024-11-29 22:56:49 -0800

    Improve the script

--- edit-uclibc-config.sh
+++ edit-uclibc-config.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+NO_FORMAT="\033[0m"
+C_GREEN="\033[38;5;2m"
+
+echo -e "${C_GREEN}Start uClibc configuration editor${NO_FORMAT}"
 make -C ./buildroot/ BR2_EXTERNAL=../br2 O=../build/br uclibc-menuconfig
+echo -e "${C_GREEN}Save uClibc configuration${NO_FORMAT}"
+make -C ./buildroot/ BR2_EXTERNAL=../br2 O=../build/br uclibc-update-config
