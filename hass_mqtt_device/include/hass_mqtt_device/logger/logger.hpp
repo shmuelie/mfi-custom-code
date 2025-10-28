9390fc0c296aac011e41361190edc73a0bc9c03c
Shmueli Englard <shmueli.yosef@englard.net>
2025-10-27 21:54:18 -0700

    Change to single line

--- hass_mqtt_device/include/hass_mqtt_device/logger/logger.hpp
+++ hass_mqtt_device/include/hass_mqtt_device/logger/logger.hpp
@@ -16,13 +16,8 @@
   spdlog::set_level(debug ? spdlog::level::trace : spdlog::level::info)
 
 // Logging macros
-#define LOG_INFO(...) spdlog::log(spdlog::source_loc(__FILE__, __LINE__, __func__), \
-						spdlog::level::level_enum::info, __VA_ARGS__)
-#define LOG_WARN(...) spdlog::log(spdlog::source_loc(__FILE__, __LINE__, __func__), \
-						spdlog::level::level_enum::warn, __VA_ARGS__)
-#define LOG_ERROR(...) spdlog::log(spdlog::source_loc(__FILE__, __LINE__, __func__), \
-						spdlog::level::level_enum::err, __VA_ARGS__)
-#define LOG_DEBUG(...) spdlog::log(spdlog::source_loc(__FILE__, __LINE__, __func__), \
-						spdlog::level::level_enum::debug, __VA_ARGS__)
-#define LOG_TRACE(...) spdlog::log(spdlog::source_loc(__FILE__, __LINE__, __func__), \
-						spdlog::level::level_enum::trace, __VA_ARGS__)
+#define LOG_INFO(...) spdlog::log(spdlog::source_loc(__FILE__, __LINE__, __func__), spdlog::level::level_enum::info, __VA_ARGS__)
+#define LOG_WARN(...) spdlog::log(spdlog::source_loc(__FILE__, __LINE__, __func__), spdlog::level::level_enum::warn, __VA_ARGS__)
+#define LOG_ERROR(...) spdlog::log(spdlog::source_loc(__FILE__, __LINE__, __func__), spdlog::level::level_enum::err, __VA_ARGS__)
+#define LOG_DEBUG(...) spdlog::log(spdlog::source_loc(__FILE__, __LINE__, __func__), spdlog::level::level_enum::debug, __VA_ARGS__)
+#define LOG_TRACE(...) spdlog::log(spdlog::source_loc(__FILE__, __LINE__, __func__), spdlog::level::level_enum::trace, __VA_ARGS__)
