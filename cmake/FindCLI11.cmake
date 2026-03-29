e5ad8eddabd745bd0f1975561362d2c227422851
Shmueli Englard <shmueli.yosef@englard.net>
2026-03-28 18:41:57 -0700

    Update dependencies

--- cmake/FindCLI11.cmake
+++ cmake/FindCLI11.cmake
@@ -2,6 +2,6 @@ include(FetchContent)
 FetchContent_Declare(
     cli11_proj
     GIT_REPOSITORY https://github.com/CLIUtils/CLI11.git
-    GIT_TAG 291c58789c031208f08f4f261a858b5b7083e8e2 #v2.3.2
+    GIT_TAG 37bb6edc5317e99af72ef48405e65d9ca5218861 #v2.6.2
 )
 FetchContent_MakeAvailable(cli11_proj)
--- cmake/Findmongoose.cmake
+++ cmake/Findmongoose.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 FetchContent_Declare(
 	mongoose
 	GIT_REPOSITORY https://github.com/cesanta/mongoose.git
-	GIT_TAG        52997c6c5170050148d23436d2ed398811f48c30 # 7.13
+	GIT_TAG        236724ea2948396b4f83b68c366a169e795aa7ef # 7.20
 )
 FetchContent_MakeAvailable(mongoose)
 set(MONGOOSE_DIR ${FETCHCONTENT_BASE_DIR}/mongoose-src/)
